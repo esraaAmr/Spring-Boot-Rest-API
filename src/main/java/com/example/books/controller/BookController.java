7ae2f9fa98b19ac847497b93dc6c78837b9d7096
esraaAmr <esraaamrkandil@gmail.com>
2025-06-25 06:24:12 -0800

    Exception Handler

--- src/main/java/com/example/books/controller/BookController.java
+++ src/main/java/com/example/books/controller/BookController.java
@@ -1,12 +1,22 @@
 package com.example.books.controller;
 
 import com.example.books.entity.Book;
+import com.example.books.exception.BookErrorResponse;
+import com.example.books.exception.BookNotFoundException;
 import com.example.books.request.BookRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Tag(name = "Books Rest API Endpoints",description = "Operations Related to Books")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -27,9 +37,11 @@ public class BookController {
         ));
     }
 
-
+    @Operation(summary = "Get all books",description = "Retrieve a list of all available books")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<Book> getBooks(@RequestParam(required = false) String category){
+    public List<Book> getBooks(@Parameter(description = "Optional query parameter")
+                                   @RequestParam(required = false) String category){
     if (category==null){
         return books;
     }
@@ -39,37 +51,60 @@ public class BookController {
     }
 
 
+
+    @Operation(summary = "Get book by Id",description = "Retrieve a specific book by Id")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Book getBookById(@PathVariable long id){
+    public Book getBookById(@Parameter(description = "Id of a book to be retrieved")
+                                @PathVariable @Min(1) long id){
         return books.stream()
                 .filter(book -> book.getId()==id)
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new BookNotFoundException("Book not found with Id - "+id));
     }
 
+
+    @Operation(summary = "Create a new book",description = "Add a new book to the list")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createBook(@RequestBody BookRequest bookRequest){
+    public void createBook(@Valid @RequestBody BookRequest bookRequest){
         long id = books.isEmpty() ? 1 : books.getLast().getId()+1;
         Book book = convertToBook(id,bookRequest);
         books.add(book);
     }
 
+    
+    @Operation(summary = "Update a book",description = "update a details of an existing book")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void updateBook(@PathVariable long id, @RequestBody BookRequest bookRequest) {
+    public Book updateBook(@Parameter(description = "Id of a book to update")
+                               @PathVariable  @Min(1) long id,
+                           @Valid @RequestBody BookRequest bookRequest) {
         for (int i=0;i<books.size();i++){
             if (books.get(i).getId()==id){
                 Book updatedBook = convertToBook(id,bookRequest);
                 books.set(i,updatedBook);
+                return updatedBook;
             }
         }
+        throw new BookNotFoundException("Book not found with Id - "+id);
     }
 
 
+    @Operation(summary = "Delete a book",description = "Delete a book from the list")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable long id){
+    public void deleteBook(@Parameter(description = "Id of a book to delete")
+                               @PathVariable @Min(1) long id){
+        books.stream()
+                .filter(book -> book.getId()==id)
+                .findFirst()
+                .orElseThrow(() -> new BookNotFoundException("Book not found with Id - "+id));
+
         books.removeIf(book -> book.getId()==id);
 
     }
+
 
     private Book convertToBook(long id , BookRequest bookRequest){
         return new Book(id,bookRequest.getTitle(),bookRequest.getAuthor(),bookRequest.getCategory(),bookRequest.getRating());
--- src/main/java/com/example/books/request/BookRequest.java
+++ src/main/java/com/example/books/request/BookRequest.java
@@ -1,10 +1,23 @@
 package com.example.books.request;
 
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
 public class BookRequest {
 
+    @Size(min = 1,max = 30,message = "Title is between 1 and 30")
     private String title;
+
+    @Size(min = 1,max = 40,message = "Author is between 1 and 40")
     private String author;
+
+    @Size(min = 1,max = 30,message = "Category is between 1 and 30")
     private String category;
+
+    @Min(value = 1,message = "Rating must be at least 1")
+    @Max(value = 5,message = "Rating cannot go pass 5")
     private int rating;
 
     public BookRequest(String title, String author, String category, int rating) {
